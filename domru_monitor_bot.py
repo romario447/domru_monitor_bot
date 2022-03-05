1e30549d5388a1966f7341bf9404e1f33fc0a97a
romario447 <85133407+romario447@users.noreply.github.com>
2022-03-05 14:30:41 +0300

    Create domru_monitor_bot.py

---
+++ domru_monitor_bot.py
@@ -0,0 +1,15 @@
+from grab import Grab
+from datetime import datetime, date
+
+# now_date = datetime.today()                         # Получаем текущую дату    
+# cur_year = str(now_date.year)                       # Год текущий
+# cur_month = str(now_date.month)                     # Месяц текущий
+# cur_day = str(now_date.day)                         # День текущий
+
+#url = 'https://my.kvant-telecom.ru/calls/?&period_start=' + cur_year + '-' + cur_month + '-' + cur_day + '&period_end=' + cur_year + '-' + cur_month + '-' + cur_day # Формируем ссылку с детализацией за сегодняшний день
+g = Grab()                                          # Инициализация объекта для граббинга страницы
+g.go('https://lkz.ahml.ru/borrower/')          # Авторизация на сайте детализации Квант-телеком
+g.set_input_by_id('b_auth_login', '00087432')     # Вводим логин
+g.set_input_by_id('b_auth_password', 'GSQ3SSq4')    # Водим пароль
+g.submit()                                          # Отсылаем форму
+g.go(url) 
